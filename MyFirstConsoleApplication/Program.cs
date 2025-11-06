b0786d952617b2c960c229e75aa90405b57d2f2d
Rajufullstack04 <rajufullstack777@gmail.com>
2025-11-06 20:34:46 +0530

    Add project files.

---
+++ MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,90 @@
+﻿
+
+using System;
+using System.CodeDom;
+class MyProgram
+    {
+        static void Main()
+        {
+        Console.WriteLine("Heloo raju.......!");
+        Console.WriteLine("Here we leren C#.net...... ");
+        Console.WriteLine("To devlop a console application...");
+
+        // datatypes 
+        /*  Numerical DT ,=> 2 types 
+           1. Floting DT
+           2.Non-Floting DT
+        -------------------------------------------------------
+         Boolean DT,
+         Charecter DT,
+        --------------------------------
+         Others 
+         Object,
+         dynamic,
+         void
+        
+         */
+
+
+        /*numerical datatypes    numbers
+         non-floting DT
+        byte , short , ushort , int , unit, long , ulong
+
+
+        */
+
+        byte age = 25;
+        Console.WriteLine(age);
+
+        short Salary = 30000;
+        Console.WriteLine(Salary);
+
+
+        ushort price = 60000;
+        Console.WriteLine(price);
+
+
+        int value = 1999;
+        Console.WriteLine("Value is :" +value);
+
+
+        uint value1 = 884U;
+        Console.WriteLine(value1);
+
+
+        long value2 = 144444444445555555l;
+        Console.WriteLine(value2);
+
+        ulong value3 = 132344354565767899Ul;
+        Console.WriteLine(value3);
+        //------------------------------------------------------------------------------------------
+        /* floting data types
+         * decimal,
+         * flot,
+         * double
+         * 
+          */
+
+        decimal price1 = 125.6M;
+        Console.WriteLine(price1);
+
+
+        float pi = 3.14F;
+        Console.WriteLine(pi);
+
+
+        double DB = 89808909808.7979797D;
+        Console.WriteLine(DB);
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
