782faa3317610d67efcbaceab732d49abd532d98
Rajufullstack04 <rajufullstack777@gmail.com>
2025-11-13 21:59:30 +0530

    methods

---
+++ MyFirstConsoleApplication/12.Mathod_Basic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Method_Basic
+{
+    static void Main()
+    {
+        // collection of statements.
+        // executes as a single unit.
+
+        //Entry point Main
+
+        // i can call method inside of the Main Method
+
+        AllMethods();
+
+    }
+
+    //Void - Non-return type
+    static void TestMethod1()
+    {
+        Console.WriteLine("Fired !!");
+    }
+
+    //Void - Non-return type
+    static void TestMethod2()
+    {
+        Console.WriteLine("One more time Fired !!");
+    }
+
+
+    //Void - Non-return type
+    static void AllMethods()
+    {
+        TestMethod1();
+        TestMethod2();
+    }
+
+
+}
+
+
